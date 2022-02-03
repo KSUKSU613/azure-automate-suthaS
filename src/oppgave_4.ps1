80d15b1fb1de71606070885dd7002491988ac738
KSUKSU613 <kanesusuthakaran@gmail.com>
2022-02-03 09:58:57 +0100

    gjort oppgave 4

---
+++ src/oppgave_4.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $UrlKortstokk = "http://nav-deckofcards.herokuapp.com/shuffle"
+)
+
+$ErrorActionPreference = 'Stop'
+
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
+
+
+$kortstokkJson = $webRequest.Content | ConvertFrom-Json
+
+$kortstokk = @()
+foreach ($kort in $kortstokkJson)
+{
+    $kortstokk +=  ($kort.suit[0] + $kort.value + ",")
+}
+
+Write-Host "Kortstokk:  $kortstokk"
